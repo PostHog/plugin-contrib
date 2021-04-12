8e400cb51de333cb568e35d949f45dacf9715acc
Marius Andra <marius.andra@gmail.com>
2021-04-12 13:58:06 +0200

    add timeout for buffer

--- src/buffer.ts
+++ src/buffer.ts
@@ -1,14 +1,17 @@
 type BufferOptions = {
-    limit: number,
+    limit: number
+    timeoutSeconds: number
     onFlush: (objects: any[], points: number) => void | Promise<void>
 }
 
 export function createBuffer(opts: Partial<BufferOptions>) {
     const buffer = {
         _buffer: [] as any[],
+        _timeout: null as NodeJS.Timeout | null,
         _points: 0,
         _options: {
             limit: 10,
+            timeoutSeconds: 60,
             ...opts,
         } as BufferOptions,
         add: (object: any, points = 1) => {
@@ -18,8 +21,15 @@ export function createBuffer(opts: Partial<BufferOptions>) {
                 buffer._points += points
                 buffer._buffer.push(object)
             }
+            if (!buffer._timeout) {
+                buffer._timeout = setTimeout(() => buffer.flush(), buffer._options.timeoutSeconds * 1000)
+            }
         },
         _flush: (object = null, points = 0) => {
+            if (buffer._timeout) {
+                clearTimeout(buffer._timeout)
+                buffer._timeout = null
+            }
             if (buffer._buffer.length > 0 || buffer._points !== 0) {
                 const oldBuffer = buffer._buffer
                 const oldPoints = buffer._points
