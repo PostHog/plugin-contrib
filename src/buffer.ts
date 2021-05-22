ca3a2201b6014d6aa514c6939c19e555ed56cfde
Marius Andra <marius.andra@gmail.com>
2021-05-22 21:42:41 +0200

    fix error when buffer lost some events if the first event > limit :)

--- src/buffer.ts
+++ src/buffer.ts
@@ -15,17 +15,24 @@ export function createBuffer(opts: Partial<BufferOptions>) {
             ...opts,
         } as BufferOptions,
         add: (object: any, points = 1) => {
-            if (buffer._points + points > buffer._options.limit) {
-                buffer._flush(object, points)
-            } else {
-                buffer._points += points
-                buffer._buffer.push(object)
+            // flush existing if adding would make us go over the limit
+            if (buffer._points && buffer._points + points > buffer._options.limit) {
+                buffer._flush()
             }
-            if (!buffer._timeout) {
+
+            // add the object to the buffer
+            buffer._points += points
+            buffer._buffer.push(object)
+
+            if (buffer._points > buffer._options.limit) {
+                // flush (again?) if we are now over the limit
+                buffer._flush()
+            } else if (!buffer._timeout) {
+                // if not, make sure there's a flush timeout
                 buffer._timeout = setTimeout(() => buffer.flush(), buffer._options.timeoutSeconds * 1000)
             }
         },
-        _flush: (object = null, points = 0) => {
+        _flush: () => {
             if (buffer._timeout) {
                 clearTimeout(buffer._timeout)
                 buffer._timeout = null
@@ -33,9 +40,9 @@ export function createBuffer(opts: Partial<BufferOptions>) {
             if (buffer._buffer.length > 0 || buffer._points !== 0) {
                 const oldBuffer = buffer._buffer
                 const oldPoints = buffer._points
-                buffer._buffer = object ? [object] : []
-                buffer._points = points
-                void buffer._options.onFlush?.(oldBuffer, oldPoints)
+                buffer._buffer = []
+                buffer._points = 0
+                setImmediate(() => buffer._options.onFlush?.(oldBuffer, oldPoints))
             }
         },
         flush: () => buffer._flush(),
